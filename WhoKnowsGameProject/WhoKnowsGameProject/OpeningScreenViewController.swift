6c256a0d2f25c6118bf5dffe3b397685d1d504b1
bkabadayi <berkinkabadayi@gmail.com>
2021-01-06 22:50:45 +0300

    "Opening Screen" as first screen is created in "main.storyboard"

---
+++ WhoKnowsGameProject/WhoKnowsGameProject/OpeningScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoKnowsGameProject
+//
+//  Created by Berkin KABADAYI on 6.01.2021.
+//
+
+import UIKit
+
+class OpeningScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
